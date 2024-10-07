8c897be218ff5217bad9931842dbb333dc284349
Ian Fraser <iandfraser8@gmail.com>
2024-10-07 20:33:12 +1000

    fixed ordering to correctly display and calculate

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-let symbol;
-let answer = 0;
-let input = 1;
+let symbol; // most recently pressed operation symbol +,-,* etc
+let answer = 0; // stored result of previous operations
+let displayValue = "";
+let input = ""; // amount to calculate against answer
+
 window.onload = ()=>{
     const plusButton = document.getElementById("plus");
     const minusButton = document.getElementById("-");
@@ -9,23 +11,39 @@ window.onload = ()=>{
 
     display.innerHTML = answer;
 
+    for (let i = 0; i <=9; i++) {
+        
+        const numbtn = document.getElementById(i.toString());
+        numbtn.onclick = () => {
+            input += i.toString();
+            display.innerHTML = input;
+        }
+    }
+
     plusButton.onclick = ()=> {
         symbol = "+";
+        display.innerHTML = input;
+        input = "";
+        
     }
 
     minusButton.onclick = ()=> {
         symbol = "-";
+        display.innerHtml = input;
+        input = "";
     }
 
     equalsButton.onclick = ()=> {
+        console.log(input);
         console.log(symbol);
-        if (symbol = "+") {
-            answer += input;
+        if (symbol == "+") {
+            answer += Number(input);
 
-        } else if (symbol = "-") {
-            answer -= input;
+        } else if (symbol == "-") {
+            answer -= Number(input);
         }
         display.innerHTML = answer;
+        
     }
     
 }
